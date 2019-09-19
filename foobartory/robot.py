4aae85b08dafae897d3bec178420500f9541d53e
Nicolas Landier <nicolas.landier@gmail.com>
2019-09-19 21:06:13 +0200

    Rename Robot._sleep() -> Robot._get_busy_for()

--- foobartory/robot.py
+++ foobartory/robot.py
@@ -10,25 +10,27 @@ TEST = os.environ.get('TEST', None)
 class Robot:
     def __init__(self):
         self.warehouses = defaultdict(lambda: 0)
+        self._nb_actions = 0
 
-    def _sleep(self, duration):
+    def _get_busy_for(self, duration):
+        self._nb_actions += 1
         if TEST is None:
             time.sleep(duration)
 
     def move(self):
-        self._sleep(5)
+        self._get_busy_for(5)
 
     def mine_foo(self):
-        self._sleep(1)
+        self._get_busy_for(1)
         self.warehouses['foo'] += 1
 
     def mine_bar(self):
         mining_duration = uniform(0.5, 2)
-        self._sleep(mining_duration)
+        self._get_busy_for(mining_duration)
         self.warehouses['bar'] += 1
 
     def assemble_foobar(self):
-        self._sleep(2)
+        self._get_busy_for(2)
         if randrange(100) < 60:
             self.warehouses['foo'] -= 1
             self.warehouses['bar'] -= 1
